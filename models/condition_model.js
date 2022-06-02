96f697c400c7a2a44adcb4e3a995e0301ce2e9a8
gala <ajory.it@gmail.com>
2022-06-02 23:43:07 +0200

    sherif

--- models/condition_model.js
+++ models/condition_model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 
-const chatSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
     title_ar: { type: String, required: true },
     title_en: { type: String, required: true },
     body_ar: { type: String, required: true },
     body_en: { type: String, required: true },
     type: { type: Number, default: 1 },
 }, { versionKey: false, timestamps: true, })
-module.exports = mongoose.model("chat", chatSchema);
+module.exports = mongoose.model("conditions", schema);
